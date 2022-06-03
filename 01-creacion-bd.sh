855327b4870dd410b5250e35e654595af44e1481
Kevin Arturo Lara Sala <kevin-lara@pc-kals.fi.unam>
2022-06-03 08:29:27 -0500

    Add project requirements

---
+++ 01-creacion-bd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#	@Autores: 		Lara Sala Kevin Arturo, Mirando Cortés Yak Balam.
+# @Fecha:				1 Junio 2022.
+#	@Descripción:	Script que generará la base de datos para el proyecto final de
+#								la asignatura de Bases de Datos Avanzadas, para el caso de
+#								estudio: House Services.
+
+
