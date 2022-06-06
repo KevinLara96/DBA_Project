151817becb1c066b17a863692de75af7c3dcd97d
YakBalam <yakmiranda3c113@gmail.com>
2022-06-06 17:22:00 -0500

    actualizacion de directorios

--- s-05-crea-directorios-root.sh
+++ s-05-crea-directorios-root.sh
@@ -5,14 +5,87 @@
 #               - Directorio para archivos Redo Logs
 #               - Directorio para archivos Control Files
 
-#Creacion de dir para Data Files
 export ORACLE_SID=lamiproy
-cd /u01/app/oracle/oradata
-mkdir ${ORACLE_SID^^}
-chown oracle:oinstall ${ORACLE_SID^^}
-chmod 750 ${ORACLE_SID^^}
 
-#Crecion de directorios para archivos de control multiplexados y grupos Redo Logs
+# Creacion de dir para Data Files
+cd /unam-bda
+mkdir -p d14/app/oracle/oradata/LAMIPROY
+# ts_provedor (disco 1)
+cd /unam-bda/d14
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d15/app/oracle/oradata/LAMIPROY
+# ts_provedor (disco 2)
+cd /unam-bda/d15
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d16/app/oracle/oradata/LAMIPROY
+# ts_p_docs-fotos (Provedor)
+cd /unam-bda/d16
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d17/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d17
+# ts_p_indices
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d18/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d18
+# ts_p_foto-servicio
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d19/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d19
+# ts_cliente (disco 1)
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d20/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d20
+# ts_cliente (disco 2)
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d21/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d21
+# ts_c_docs-fotos (Cliente)
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d22/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d22
+# ts_c_indices
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d23/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d23
+# ts_undo
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+cd /unam-bda
+mkdir -p d24/app/oracle/oradata/LAMIPROY
+cd /unam-bda/d24
+# ts_fra
+chown -R oracle:oinstall app
+chmod -R 750 app
+
+# Crecion de directorios para archivos de control multiplexados y grupos Redo Logs
 cd /unam-bda/d11
 mkdir -p app/oracle/oradata/LAMIPROY
 chown -R oracle:oinstall app
