3be13b8746eba8a7cc765d61adc07ee1ea8029dd
YakBalam <yakmiranda3c113@gmail.com>
2022-06-09 19:17:58 -0500

    correccion de tablespaces

--- s-08-crea-tbs.sql
+++ s-08-crea-tbs.sql
@@ -10,39 +10,64 @@ whenever sqlerror exit rollback;
 
 
 --Proveedor.
-create tablespace ts_proveedor1
-datafile '/unam-bda/d14/app/oracle/oradata/LAMIPROY/ts_proveedor1.dbf'
-  size 500m reuse autoextend on next 50m maxsize unlimited;
-create tablespace ts_proveedor2
-datafile '/unam-bda/d15/app/oracle/oradata/LAMIPROY/ts_proveedor2.dbf'
-  size 500m reuse autoextend on next 50m maxsize unlimited;
+create tablespace ts_provedor
+datafile '/unam-bda/d14/app/oracle/oradata/LAMIPROY/ts_provedor1.dbf'
+  size 250m reuse autoextend on next 50m maxsize unlimited,
+  '/unam-bda/d15/app/oracle/oradata/LAMIPROY/ts_provedor2.dbf'
+  size 250m reuse autoextend on next 50m maxsize unlimited
+extent management local autoallocate
+segment space management auto
+online;
+
 --Documentos proveedor.
-create tablespace ts_prov_docs-fotos
-datafile '/unam-bda/d16/app/oracle/oradata/LAMIPROY/ts_prov_docs-fotos.dbf'
-  size 700m reuse autoextend on next 100m maxsize unlimited;
+create tablespace ts_p_docs_fotos
+datafile '/unam-bda/d16/app/oracle/oradata/LAMIPROY/ts_p_docs_fotos.dbf'
+  size 700m reuse autoextend on next 100m maxsize unlimited
+extent management local autoallocate
+segment space management auto
+online;
+
 --Índices proveedor.
-create tablespace ts_prov_indices
-datafile '/unam-bda/d17/app/oracle/oradata/LAMIPROY/ts_prov_indices.dbf'
-  size 400m reuse autoextend on next 10m maxsize unlimited;
+create tablespace ts_p_indices
+datafile '/unam-bda/d17/app/oracle/oradata/LAMIPROY/ts_p_indices.dbf'
+  size 400m reuse autoextend on next 10m maxsize unlimited
+extent management local autoallocate
+segment space management auto
+online;
+
 --Fotos servicio.
-create tablespace ts_prov_foto-servicio
-datafile '/unam-bda/d18/app/oracle/oradata/LAMIPROY/ts_prov_foto-servicio.dbf'
-  size 700m reuse autoextend on next 700m maxsize unlimited;
+create tablespace ts_p_foto_servicio
+datafile '/unam-bda/d18/app/oracle/oradata/LAMIPROY/ts_p_foto_servicio.dbf'
+  size 700m reuse autoextend on next 700m maxsize unlimited
+extent management local autoallocate
+segment space management auto
+online;
+
 --Cliente.
-create tablespace ts_cliente1
+create tablespace ts_cliente
 datafile '/unam-bda/d19/app/oracle/oradata/LAMIPROY/ts_cliente1.dbf'
-  size 500m reuse autoextend on next 50m maxsize unlimited;
-create tablespace ts_cliente2
-datafile '/unam-bda/d20/app/oracle/oradata/LAMIPROY/ts_cliente2.dbf'
-  size 500m reuse autoextend on next 50m maxsize unlimited;
+  size 250m reuse autoextend on next 50m maxsize unlimited,
+  '/unam-bda/d20/app/oracle/oradata/LAMIPROY/ts_cliente2.dbf'
+  size 250m reuse autoextend on next 50m maxsize unlimited;
+extent management local autoallocate
+segment space management auto
+online;
+
 --Documentos cliente.
-create tablespace ts_cli_docs-photos
-datafile '/unam-bda/d21/app/oracle/oradata/LAMIPROY/ts_cli_docs-fotos.dbf'
-  size 700m reuse autoextend on next 100m maxsize unlimited;
+create tablespace ts_c_docs_fotos
+datafile '/unam-bda/d21/app/oracle/oradata/LAMIPROY/ts_c_docs_fotos.dbf'
+  size 700m reuse autoextend on next 100m maxsize unlimited,
+  extent management local autoallocate
+  segment space management auto
+  online;
+
 --Índices cliente.
-create tablespace ts_cli_indices
-datafile '/unam-bda/d22/app/oracle/oradata/LAMIPROY/ts_cli_indices.dbf'
-  size 400m reuse autoextend on next 10m maxsize unlimited;
+create tablespace ts_c_indices
+datafile '/unam-bda/d22/app/oracle/oradata/LAMIPROY/ts_c_indices.dbf'
+  size 400m reuse autoextend on next 10m maxsize unlimited
+  extent management local autoallocate
+  segment space management auto
+  online;
 
 --Users. (Usar si se necesita).
 --  default tablespace users
