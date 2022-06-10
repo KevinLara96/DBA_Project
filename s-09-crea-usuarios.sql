e488a6d668a6f50ae7eb4c5d2e99575544099b8d
YakBalam <yakmiranda3c113@gmail.com>
2022-06-10 13:18:37 -0500

    correcion del usuario sysbackup

--- s-09-crea-usuarios.sql
+++ s-09-crea-usuarios.sql
@@ -50,20 +50,6 @@ default tablespace ts_cliente;
 grant create session, create table, create sequence, create trigger, create procedure to cliente;
 
 
--- Creacion del usuario SYSBACKUP
-declare
-v_count number;
-v_username varchar2(20) := 'SYSBACKUP';
+-- Desbloqueo del usuario SYSBACKUP
 
-begin
-select count(*) into v_count from all_users where username=v_username;
-if v_count >0 then
-execute immediate 'drop user '||v_username|| ' cascade';
-end if;
-end;
-/
-Prompt creando al usuario SYSBACKUP
-create user sysbackup identified by system3
-quota unlimited on xxxxx;
-
-grant create session, sysbackup to sysbackup;
+alter user sysbackup account unlock;
